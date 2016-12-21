1249dd55a4bd2bf5897786c24f28d319444ea5b4
hunbernd <hunbernd@outlook.com>
2016-12-21 01:27:07 +0100

    Added request for chat messages

---
+++ RetroShareApi/Request/Chat/Messages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroShareApi.Connection;
+using RetroShareApi.Response;
+using RetroShareApi.Response.Chat;
+
+namespace RetroShareApi.Request.Chat
+{
+    public class Messages : Request<List<Response.Chat.Message>>
+    {
+        public Messages(string chatid) : base("chat", "messages/" + chatid) { }
+
+        private string begin_after = "begin";
+
+        public override Response<List<Message>> Execute(IConnection connection)
+        {
+            this.data = "{\"begin_after\":\"" + begin_after + "\"}";
+            Response<List<Message>> answer = base.Execute(connection);
+            if(answer.data.Count > 0)
+            {
+                begin_after = answer.data.Last().id;
+            }
+            return answer;
+        }
+    }
+}

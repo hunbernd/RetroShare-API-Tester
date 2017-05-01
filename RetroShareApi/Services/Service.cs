de40fcd8b049dfbe192a1bf293d7da4057a5ae35
hunbernd <hunbernd@outlook.com>
2017-05-01 23:15:19 +0200

    Added high level API

---
+++ RetroShareApi/Services/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroShareApi.Services
+{
+    public abstract class Service
+    {
+        public readonly ServiceManager serviceManager;
+
+        Service(ServiceManager servicemanager)
+        {
+            this.serviceManager = servicemanager;
+            
+        }      
+    }
+}
---
+++ RetroShareApi/Services/ServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using RetroShareApi.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroShareApi.Services
+{
+    public class ServiceManager
+    {
+        public readonly IConnection connection;
+
+        private Dictionary<Type, Service> servicecache;
+
+        public ServiceManager(IConnection connection)
+        {
+            this.connection = connection;
+            servicecache = new Dictionary<Type, Service>();
+        }
+
+        public T getService<T>() where T : Service, new()
+        {
+            if (servicecache.ContainsKey(typeof(T)))
+            {
+                return (T)servicecache[typeof(T)];
+            }
+            else
+            {
+                T service = new T();
+                servicecache.Add(typeof(T), service);
+                return service;
+            }
+        }
+    }
+}
